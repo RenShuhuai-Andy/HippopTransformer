0757801542f5a4855f96b914189c7e69da444466
RenShuhuai-Andy <739584819@qq.com>
2021-06-22 20:58:34 +0800

    add script for transformer-gan

---
+++ scripts/train_transformer_gan.sh
@@ -0,0 +1,26 @@
+model_signature=transformer_gan_base
+GPU="4,5,6,7"
+save_tag=${model_signature}
+
+CUDA_VISIBLE_DEVICES=$GPU fairseq-train \
+    data/data-bin --share-all-embeddings \
+    --user-dir task --fp16  \
+    --arch $model_signature \
+    --task gan_hippop_translation \
+    --optimizer adam --adam-betas '(0.9,0.98)' --clip-norm 0.0 \
+    --save-dir checkpoints/$save_tag \
+    --max-update 25000 --save-interval-updates 2000  --validate-interval 3 \
+    --keep-interval-updates 10 \
+    --keep-best-checkpoints 10 \
+    --no-epoch-checkpoints \
+    --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
+    --weight-decay 0.0001 \
+    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --dropout 0.3 \
+    --max-tokens 4096 \
+    --tensorboard-logdir tensorboard-logdir/$save_tag \
+    --eval-bleu \
+    --eval-bleu-args '{"beam": 4, "max_len_a": 1.2, "max_len_b": 10}' \
+    --eval-bleu-detok moses \
+    --eval-bleu-remove-bpe \
+    --best-checkpoint-metric bleu --maximize-best-checkpoint-metric 2>&1 | tee -a logs/$save_tag.log
+
